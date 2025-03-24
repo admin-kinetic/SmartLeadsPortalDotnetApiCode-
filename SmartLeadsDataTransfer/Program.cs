8a74d2bebac261d8b9323478c60f22822f1b173c
Garry Agum <gjagum@kineticstaff.com>
2025-03-24 15:48:42 +0800

    Add DataTransferService for migrating data from MySQL to SQL Server

---
+++ SmartLeadsDataTransfer/Program.cs
@@ -0,0 +1,12 @@
+﻿using SmartLeadsDataTransfer;
+
+
+Console.WriteLine("Data transfer started.");
+
+var mysqlConnectionString = "Server=kis-systems.com;Database=AutomatedLeadsDb;User ID=AutomatedLeadsDb_User;Password=\"Es8]jg[EB)D;\";";
+var sqlServerConnectionString = "Server=localhost,1433;Database=SmartLeadsPortal;User ID=sa;Password=Test@123;Encrypt=True;TrustServerCertificate=True;";
+
+var transferService = new DataTransferService(mysqlConnectionString, sqlServerConnectionString);
+await transferService.TransferData(5000, 45000, 45000);
+
+Console.WriteLine("Data transfer completed successfully.");
