895727d228947480d98a310f461d59aabf701366
Leo Dizon <123930496+leokinetic@users.noreply.github.com>
2025-03-26 15:44:39 +0800

    add VOIP config and API Service

---
+++ SmartLeadsPortalDotNetApi/Controllers/VoipController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartLeadsPortalDotNetApi.Services;
+
+namespace SmartLeadsPortalDotNetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoipController : ControllerBase
+    {
+        private readonly VoipHttpService _voipHttpService;
+        public VoipController(VoipHttpService voipHttpService)
+        {
+            _voipHttpService = voipHttpService;
+        }
+
+        [HttpGet("GetUserCalls")]
+        public async Task<IActionResult> GetVoipData()
+        {
+            var voipData = await _voipHttpService.GetVoipData();
+            return Ok(voipData);
+        }
+    }
+}
