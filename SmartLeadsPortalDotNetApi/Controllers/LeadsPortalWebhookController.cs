2077c4e314f922f30ba8c11270b42a28beaddc7b
Garry Agum <gjagum@kineticstaff.com>
2025-05-15 15:24:42 +0800

    Add LeadsPortalWebhookRepository and integrate logging for exported contacts processing

--- SmartLeadsPortalDotNetApi/Controllers/LeadsPortalWebhookController.cs
+++ SmartLeadsPortalDotNetApi/Controllers/LeadsPortalWebhookController.cs
@@ -11,19 +11,30 @@ namespace SmartLeadsPortalDotNetApi.Controllers
     public class LeadsPortalWebhookController : ControllerBase
     {
         private readonly SmartLeadsExportedContactsRepository smartLeadsExportedContactsRepository;
+        private readonly LeadsPortalWebhookRepository leadsPortalWebhookRepository;
+        private readonly ILogger<LeadsPortalWebhookController> logger;
 
-        public LeadsPortalWebhookController(SmartLeadsExportedContactsRepository smartLeadsExportedContactsRepository)
+        public LeadsPortalWebhookController(
+            SmartLeadsExportedContactsRepository smartLeadsExportedContactsRepository, 
+            LeadsPortalWebhookRepository leadsPortalWebhookRepository,
+            ILogger<LeadsPortalWebhookController> logger)
         {
             this.smartLeadsExportedContactsRepository = smartLeadsExportedContactsRepository;
+            this.leadsPortalWebhookRepository = leadsPortalWebhookRepository;
+            this.logger = logger;
         }
 
         [HttpPost("exported-contacts")]
         public async Task<IActionResult> ExportedContacts()
         {
+            this.logger.LogInformation("Processing exported contacts");
             using var reader = new StreamReader(Request.Body);
             string payload = await reader.ReadToEndAsync();
 
+            await this.leadsPortalWebhookRepository.Insert(payload, "EXPORTED_CONTACTS");
+
             var exportedContactsPayload = JsonSerializer.Deserialize<List<ExportedContactsPayload>>(payload);
+            this.logger.LogInformation($"Recieved {exportedContactsPayload?.Count()} exported contacts");
 
             await this.smartLeadsExportedContactsRepository.SaveExportedContacts(exportedContactsPayload);
             return Ok();
