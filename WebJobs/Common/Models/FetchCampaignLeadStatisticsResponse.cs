118a331cf7680ff150d1afab4124f48c41d14134
Garry Agum <gjagum@kineticstaff.com>
2025-05-20 16:41:09 +0800

    feat: Implement EmailOpenPayload model and SmartLeadsEmailStatisticsRepository for email open statistics tracking

---
+++ WebJobs/Common/Models/FetchCampaignLeadStatisticsResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models;
+
+public class FetchCampaignLeadStatisticsResponse
+{
+    public bool hasMore { get; set; }
+    public List<Datum> data { get; set; }
+    public int skip { get; set; }
+    public int limit { get; set; }
+}
+
+
+public class Datum
+{
+    public string lead_id { get; set; }
+    public string email_lead_map_id { get; set; }
+    public string from { get; set; }
+    public string to { get; set; }
+    public string status { get; set; }
+    public DateTime created_at { get; set; }
+    public int last_seq_num { get; set; }
+    public List<History> history { get; set; }
+}
