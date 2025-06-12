f45d7803205e481a60a243e3072cf4b6b962bce1
Garry Agum <gjagum@kineticstaff.com>
2025-06-12 14:24:10 +0800

    Add new entities, converters, and repositories for lead management; implement logging and configuration updates

---
+++ WebJobs/Common/Entities/SmartleadsAccounts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Entities;
+
+public class SmartleadsAccounts
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? ApiKey { get; set; }
+    public bool? IsDeleted { get; set; }
+}
--- WebJobs/ReprocessLeadCategoryUpdated/Program.cs
+++ WebJobs/ReprocessLeadCategoryUpdated/Program.cs
@@ -2,6 +2,8 @@
 using Common.Repositories;
 using Common.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ReprocessLeadCategoryUpdated;
 
@@ -9,14 +11,34 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        Console.WriteLine("Reprocessing lead category updated webhook");
+
+        // Configure logger
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        // Create logger factory
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddSerilog();
+        });
+
+        // Create logger
+        var logger = loggerFactory.CreateLogger<Program>();
+
+
+        logger.LogInformation("Reprocessing lead category updated webhook");
         var configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json").Build();
         var dbConnectionFactory = new DbConnectionFactory(configuration);
         var smartLeadsExportedContactsRepository = new SmartLeadsExportedContactsRepository(dbConnectionFactory);
+        var smartLeadCampainsRepository = new SmartleadCampaignRepository(dbConnectionFactory);
+        var smartLeadsAllLeadsRepository = new SmartLeadsAllLeadsRepository(dbConnectionFactory, smartLeadCampainsRepository, loggerFactory.CreateLogger<SmartLeadsAllLeadsRepository>());
         var smartLeadsHttpService = new SmartLeadHttpService();
-        var reprocessLeadCategoryUpdatedService = new ReprocessLeadCategoryUpdatedService(configuration, dbConnectionFactory, smartLeadsExportedContactsRepository, smartLeadsHttpService);
+        var reprocessLeadCategoryUpdatedService = new ReprocessLeadCategoryUpdatedService(
+            configuration, dbConnectionFactory, smartLeadsExportedContactsRepository, smartLeadsAllLeadsRepository, smartLeadCampainsRepository, smartLeadsHttpService);
         await reprocessLeadCategoryUpdatedService.Run();
-        Console.WriteLine("Done reprocessing lead category updated webhook");
+        logger.LogInformation("Done reprocessing lead category updated webhook");
     }
 }
