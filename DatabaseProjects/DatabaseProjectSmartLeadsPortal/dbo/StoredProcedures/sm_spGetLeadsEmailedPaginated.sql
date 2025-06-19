3c62987c8189459e9adc1e6caa23ca82b9d29d70
Garry Agum <gjagum@kineticstaff.com>
2025-06-19 17:36:59 +0800

    Refactor lead retrieval procedures to enhance filtering logic and improve query clarity

--- DatabaseProjects/DatabaseProjectSmartLeadsPortal/dbo/StoredProcedures/sm_spGetLeadsEmailedPaginated.sql
+++ DatabaseProjects/DatabaseProjectSmartLeadsPortal/dbo/StoredProcedures/sm_spGetLeadsEmailedPaginated.sql
@@ -10,33 +10,26 @@ CREATE OR ALTER PROCEDURE [dbo].[sm_spGetLeadsEmailedPaginated]
 	@QaBy NVARCHAR(255) = NULL
 AS
 BEGIN
- SET NOCOUNT ON;
-	SELECT 
-		sal.FirstName + ' ' + sal.LastName AS FullName, 
-		sal.CompanyName, 
-		sal.Email, 
-		sal.PhoneNumber, 
-		sal.CreatedAt AS ExportedDate, 
-		ses.SequenceNumber, 
-		ses.ReplyTime, 
+	SET NOCOUNT ON;
+	SELECT
+		sal.FirstName + ' ' + sal.LastName AS FullName,
+		sal.CompanyName,
+		sal.Email,
+		sal.PhoneNumber,
+		sal.CreatedAt AS ExportedDate,
+		ses.SequenceNumber,
+		ses.ReplyTime,
 		ses.SentTime, sal.[Location] AS Country
 	FROM [dbo].[SmartLeadAllLeads] sal
-	LEFT JOIN [dbo].[SmartLeadsEmailStatistics] ses ON sal.Email = ses.LeadEmail
-	WHERE (@email = '' OR sal.Email =@email)
-		AND (@hasReply IS NULL 
-			OR (@hasReply = 1 AND (ses.ReplyTime IS NOT NULL AND LTRIM(RTRIM(CAST(ses.ReplyTime AS NVARCHAR))) <> ''))
-			OR (@hasReply = 0 AND (ses.ReplyTime IS NULL OR LTRIM(RTRIM(CAST(ses.ReplyTime AS NVARCHAR))) = '')))
-		AND ((@startDate IS NULL OR @endDate IS NULL) 
-			OR (ses.ReplyTime >= CONVERT(DATE, @startDate) 
-			AND ses.ReplyTime <= CONVERT(DATE, @endDate)
-			))
+		LEFT JOIN [dbo].[SmartLeadsEmailStatistics] ses ON sal.Email = ses.LeadEmail
+	WHERE ((@email IS NULL OR @email = '') OR sal.Email = @email)
+		AND (((@startDate IS NULL OR @endDate IS NULL) OR @hasReply IS NOT NULL) OR (sal.CreatedAt IS NOT NULL AND CONVERT(DATE, sal.CreatedAt) >= @startDate AND CONVERT(DATE, sal.CreatedAt) <= @endDate))
+		AND (((@startDate IS NULL OR @endDate IS NULL) OR @hasReply = 0) OR (ses.ReplyTime IS NOT NULL AND CONVERT(DATE, ses.ReplyTime) >= @startDate AND CONVERT(DATE, ses.ReplyTime) <= @endDate))
+		AND (@hasReply = 1 OR ses.ReplyTime IS NULL)
 		AND (@Bdr IS NULL OR sal.Bdr = @Bdr)
 		AND (@LeadGen IS NULL OR sal.CreatedBy = @LeadGen)
 		AND (@QaBy IS NULL OR sal.QaBy = @QaBy)
 	ORDER BY ses.SentTime DESC
-	OFFSET (@Page - 1) * @PageSize ROWS
-	FETCH NEXT @PageSize ROWS ONLY
-	OPTION (RECOMPILE);
 END
 GO
 
