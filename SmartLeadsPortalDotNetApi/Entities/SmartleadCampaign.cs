7dfa1fbaa9cb42493a4a1965b45adf5882e30721
Garry Agum <gjagum@kineticstaff.com>
2025-06-23 22:30:03 +0800

    Add SmartleadAccount and SmartLeadCampaign entities, and implement SmartleadAccountRepository for account management

--- SmartLeadsPortalDotNetApi/Entities/SmartleadCampaign.cs
+++ SmartLeadsPortalDotNetApi/Entities/SmartleadCampaign.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace SmartLeadsPortalDotNetApi.Entities;
 
-public class SmartleadsAccounts
+public class SmartLeadCampaign
 {
     public int? Id { get; set; }
     public string? Name { get; set; }
