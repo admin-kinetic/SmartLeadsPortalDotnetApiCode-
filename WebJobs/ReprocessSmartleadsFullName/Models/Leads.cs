6c62b8ee7dd93c6d4640710410e105680c8bc627
Garry Agum <gjagum@kineticstaff.com>
2025-07-11 01:19:41 +0800

    Add reprocess smartleads fullname webjobs

---
+++ WebJobs/ReprocessSmartleadsFullName/Models/Leads.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReprocessSmartleadsFullName.Models;
+
+internal class Leads;
+
+public class LeadsToBeUpdated {
+    public string? Email { get; set; }
+    public int AccountId { get; set; }
+    public string? AccountName { get; set; }
+    public string? AccountApiKey { get; set; }
+}
+
